258a251f2dd27a09bbdf32b75eb01a5c8273f19c
Abdullah <ayoabdullah13@gmail.com>
2024-08-31 15:59:08 +0100

    FirstCommit

---
+++ src/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./style1.css"
+
+function Nav() {
+
+  return (
+    
+        <>
+            <nav className='sidenav sidebar'>
+                <a href="">Home</a>
+                <a href="">Shirts</a>
+                <a href="">Dresses</a>
+                <a href="">Trousers</a>
+                <a href="">Jackets</a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+            </nav>
+        </>
+  )
+}
+
+export default Nav
